fae5ee38592912c6154720f479e1951597ef7254
cfehring <hirechrisf@gmail.com>
2023-05-30 10:43:08 -0700

    Change colors and Minor tweaks
    Change colors
    Edit site metadata
    Change Blog to Events on navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Bel Marin Keys Yacht Club",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "this bmkyc website is built with Gatsby and Decap CMS and deployed on Netlify. It is based on the gatsby-starter-decap-cms template.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
